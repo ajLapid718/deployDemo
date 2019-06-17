c1a17f6c54299e38e1162232f4f0aad40c936151
Allan James Lapid <ajLapid718@gmail.com>
2019-06-16 23:38:39 -0400

    Use environmental variable for port number in production

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 1234;
 
 app.get("/students", (req, res, next) => {
   res.status(200).json("All of the students!!!");
@@ -9,6 +10,6 @@ app.get("/students", (req, res, next) => {
 let locationOfPublicFolder = path.join(__dirname, "client", "build")
 app.use(express.static(locationOfPublicFolder));
 
-app.listen(1234, () => {
-  console.log("Listening on port 1234!!!");
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!!!`);
 })
