17bf367339ec94715f6ca8a33f26dff562afaada
Allan James Lapid <ajLapid718@gmail.com>
2019-06-16 22:25:48 -0400

    Track client directory

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    axios.get("/students")
+      .then(res => res.data)
+      .then(students => console.log(students))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
+      </div>
+    );
+  }
+}
+
+export default App;
